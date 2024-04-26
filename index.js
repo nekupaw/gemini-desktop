0358ea74983ee62f332f846ebb679159fb06abca
Neku <128070292+nekupaw@users.noreply.github.com>
2024-04-27 01:13:25 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const { app, Tray, Menu, shell, BrowserWindow, globalShortcut, screen} = require('electron');
-const path = require('path');
-const Store = require('electron-store');
+const { app, Tray, Menu, shell, BrowserWindow, globalShortcut, screen, systemPreferences} = require('electron');
+const { generateColorScript } = require('./generateColor.js');
 
-const store = new Store();
+const path = require('path');
+const {generateColor} = require("./generateColor");
 
 let tray;
 let win;
 let icon = path.join(__dirname, 'icon/google-gemini-icon.png');
+const accentColor = systemPreferences.getAccentColor().substring(0, 6);
+const backgroundColorAccent = generateColor(`#${accentColor}`, 0.3);
 
 const isAlreadyRunning = app.requestSingleInstanceLock();
 
@@ -17,25 +19,22 @@ if (isAlreadyRunning) {
 const createWindow = () => {
     const display = screen.getPrimaryDisplay();
 
-    const lastWinWidth = store.get('winWidth', 430);
-
     let width = display.bounds.width;
     let height = display.bounds.height;
 
     win = new BrowserWindow({
-        width: lastWinWidth,
+        width: 430,
         height: 800,
-        maxHeight: 800,
-        maxWidth: 430,
-        minHeight: 800,
-        minWidth: 400,
         show: false,
         frame: false,
         skipTaskbar: true,
         showInBackground: true,
-        x: width - 460,
+        x: width - 440,
         y: height -860,
         icon: icon,
+        maximizable: false,
+        resizable: false,
+        fullscreenable: false,
         webPreferences: {
             devTools: false
         }
@@ -43,9 +42,7 @@ const createWindow = () => {
 
     win.loadURL('https://gemini.google.com/app');
 
-    win.on('close', () => {
-        store.set('winWidth', win.getBounds().width);
-    })
+    Menu.setApplicationMenu(null)
 
     win.webContents.on('did-finish-load', () => {
         loadPreScript();
@@ -63,7 +60,7 @@ app.whenReady().then(() => {
     tray = new Tray(icon);
     const contextMenu = Menu.buildFromTemplate([
         {label: 'Quit Gemini', type: 'normal', click: () => {
-            win.close();
+                win.close();
             }},
         {label: 'about (GitHub)', type: 'normal', click: () => {
                 const url = 'https://github.com/nekupaw/gemini-desktop';
@@ -77,33 +74,64 @@ app.whenReady().then(() => {
 
     createWindow();
 
-    globalShortcut.register('Control+g', () => {
+    globalShortcut.register('Ctrl+g', () => {
         if (win.isVisible()) win.hide();
         else win.show();
     })
+
+    globalShortcut.register('Ctrl+Shift+g', () => {
+        if (!win.isVisible()) win.show();
+        win.webContents.executeJavaScript("document.querySelector('.speech_dictation_mic_button').click()");
+    })
 });
 
-function loadPreScript() {
-    win.webContents.executeJavaScript(`
+const preScript = `
+    const darkMode = document.body.classList.contains('dark-theme');
+    function a(a){
+        return document.querySelector('.'+a);
+    }
     const loginFrame = document.getElementsByClassName('gb_Ld')[0];
+        const addedTextStyle = {
+        fontFamily: 'Google Sans',
+        fontSize: '15px',
+        opacity: 0,
+        transition: 'opacity 0.4s',
+        pointerEvents: 'none'
+    }
     const openWithText = document.createElement('span');
-    openWithText.textContent = 'open Gemini with [Ctrl] + [G]';
-    openWithText.style.cssText = 'font-family: "Google Sans","Helvetica Neue",sans-serif; font-size: 15px; opacity: 0; transition: 0.2s';
-    setTimeout(() => {
-    openWithText.style.opacity = 0.5;
-    }, 600);
-
+    openWithText.textContent = "open Gemini with [Ctrl] + [G]";
+    Object.assign(openWithText.style, addedTextStyle);
+    setTimeout(() => {openWithText.style.opacity = 0.5;}, 600);
+    const openMicText = document.createElement('span');
+    openMicText.textContent = 'activate mic with [Ctrl] + [Shift] + [G]';
+    Object.assign(openMicText.style, addedTextStyle);
+    setTimeout(() => {openMicText.style.opacity = 0.5;},700);
     if (!loginFrame) {
-
         const gmatCaption = document.querySelector('.gmat-caption');
-        
-        document.querySelector('.zero-state-wrapper').appendChild(openWithText);
+        const zeroStateWrapper = document.querySelector('.zero-state-wrapper');
+        zeroStateWrapper.style.cssText = 'display: flex; flex-direction: column; gap: 10px;';
+        zeroStateWrapper.append(openWithText, openMicText);
         gmatCaption.textContent = 'Gemini Client for Windows by @nekupaw';
-        gmatCaption.style.opacity = '0.5';
+        Object.assign(gmatCaption.style, {
+            opacity: 0.5,
+            pointerEvents: 'none'
+        });
         const hello = document.querySelector('.bard-hello').textContent.split(' ');
         const greetings = ['heyaa', 'hewuu', 'hiii', 'ayy'];
         hello[0] = greetings[Math.floor(Math.random() * greetings.length)];
         document.querySelector('.bard-hello').textContent = hello.join(' ');
+        document.querySelector('.bard-hello').style.pointerEvents = 'none';
         document.querySelector('.bard-question').textContent = 'how can I help? owo';
-    }`);
+        const style = document.createElement('style');
+        style.textContent = 'body, .chat-container, .response-container, .chat-history, .mat-drawer, .mat-expansion-panel-header, .mat-expansion-panel-content  {background-color: ${backgroundColorAccent} !important} .bottom-container:before {background: linear-gradient(to top, ${backgroundColorAccent}, rgba(0, 0, 0, 0)) !important } .bottom-container, .mat-bottom-sheet-container, .bard-mode-bottom-sheet, .mat-mdc-dialog-surface {background: ${backgroundColorAccent} !important} .bard-question{pointer-events: none} .gmat-caption {pointer-events: none; !important} .text-input-field, .circular-background{background: none !important; backdrop-filter: brightness(0.7)}';
+        document.head.appendChild(style);
+        if (darkMode) {
+        const darkModeStyle = document.createElement('style');
+        darkModeStyle.textContent = '.bard-question {color: white !important; opacity: 0.4}'
+        document.head.appendChild(darkModeStyle);
+        }
+    }`
+
+function loadPreScript() {
+    win.webContents.executeJavaScript(preScript);
 }
