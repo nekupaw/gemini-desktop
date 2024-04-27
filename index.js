d0fc9baa0d8050f4154ae2b1b285a919ee3df60c
Neku <128070292+nekupaw@users.noreply.github.com>
2024-04-27 18:35:22 +0200

    bug fixes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let tray;
 let win;
 let icon = path.join(__dirname, 'icon/google-gemini-icon.png');
 const accentColor = systemPreferences.getAccentColor().substring(0, 6);
-const backgroundColorAccent = generateColor(`#${accentColor}`, 0.3);
+const backgroundColorAccent = generateColor(`#${accentColor}`, 0.2);
 
 const isAlreadyRunning = app.requestSingleInstanceLock();
 
@@ -101,35 +101,35 @@ const preScript = `
     const openWithText = document.createElement('span');
     openWithText.textContent = "open Gemini with [Ctrl] + [G]";
     Object.assign(openWithText.style, addedTextStyle);
-    setTimeout(() => {openWithText.style.opacity = 0.5;}, 600);
     const openMicText = document.createElement('span');
     openMicText.textContent = 'activate mic with [Ctrl] + [Shift] + [G]';
     Object.assign(openMicText.style, addedTextStyle);
-    setTimeout(() => {openMicText.style.opacity = 0.5;},700);
+
     if (!loginFrame) {
         const gmatCaption = document.querySelector('.gmat-caption');
         const zeroStateWrapper = document.querySelector('.zero-state-wrapper');
         zeroStateWrapper.style.cssText = 'display: flex; flex-direction: column; gap: 10px;';
         zeroStateWrapper.append(openWithText, openMicText);
         gmatCaption.textContent = 'Gemini Client for Windows by @nekupaw';
-        Object.assign(gmatCaption.style, {
-            opacity: 0.5,
-            pointerEvents: 'none'
-        });
         const hello = document.querySelector('.bard-hello').textContent.split(' ');
         const greetings = ['heyaa', 'hewuu', 'hiii', 'ayy'];
         hello[0] = greetings[Math.floor(Math.random() * greetings.length)];
         document.querySelector('.bard-hello').textContent = hello.join(' ');
         document.querySelector('.bard-hello').style.pointerEvents = 'none';
         document.querySelector('.bard-question').textContent = 'how can I help? owo';
-        const style = document.createElement('style');
-        style.textContent = 'body, .chat-container, .response-container, .chat-history, .mat-drawer, .mat-expansion-panel-header, .mat-expansion-panel-content  {background-color: ${backgroundColorAccent} !important} .bottom-container:before {background: linear-gradient(to top, ${backgroundColorAccent}, rgba(0, 0, 0, 0)) !important } .bottom-container, .mat-bottom-sheet-container, .bard-mode-bottom-sheet, .mat-mdc-dialog-surface {background: ${backgroundColorAccent} !important} .bard-question{pointer-events: none} .gmat-caption {pointer-events: none; !important} .text-input-field, .circular-background{background: none !important; backdrop-filter: brightness(0.7)}';
-        document.head.appendChild(style);
+        let o;
         if (darkMode) {
-        const darkModeStyle = document.createElement('style');
-        darkModeStyle.textContent = '.bard-question {color: white !important; opacity: 0.4}'
-        document.head.appendChild(darkModeStyle);
-        }
+            o = 0.5;
+            const style = document.createElement('style');
+            style.textContent = 'body, .chat-container, .response-container, .chat-history, .mat-drawer, .mat-expansion-panel-header, .mat-expansion-panel-content  {background-color: ${backgroundColorAccent} !important} .bottom-container:before {background: linear-gradient(to top, ${backgroundColorAccent}, rgba(0, 0, 0, 0)) !important } .bottom-container, .mat-bottom-sheet-container, .bard-mode-bottom-sheet, .mat-mdc-dialog-surface {background: ${backgroundColorAccent} !important} .bard-question{pointer-events: none} .gmat-caption {pointer-events: none; !important} .text-input-field, .circular-background{background: none !important; backdrop-filter: brightness(0.7)} .bard-question {color: white !important; opacity: 0.4}';
+            document.head.appendChild(style);
+        } else o = 0.8;
+        Object.assign(gmatCaption.style, {
+            opacity: o,
+            pointerEvents: 'none'
+        });
+        setTimeout(() => {openWithText.style.opacity = o;}, 600);
+        setTimeout(() => {openMicText.style.opacity = o;}, 700);
     }`
 
 function loadPreScript() {
